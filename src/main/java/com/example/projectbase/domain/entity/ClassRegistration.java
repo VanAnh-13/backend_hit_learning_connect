7d7a84481b07172501504ba844db83f9f04a4bb6
thachpham2103 <phamthach2103@gmail.com>
2025-06-27 17:16:19 +0700

    mapper

--- src/main/java/com/example/projectbase/domain/entity/ClassRegistration.java
+++ src/main/java/com/example/projectbase/domain/entity/ClassRegistration.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class ClassRegistration {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/projectbase/domain/mapper/ClassRegistrationMapper.java
@@ -0,0 +1,19 @@
+package com.example.projectbase.domain.mapper;
+
+import com.example.projectbase.domain.entity.ClassRegistration;
+import com.example.projectbase.domain.dto.response.classes.ClassRegistrationResponse;
+
+import java.time.LocalDateTime;
+
+public interface ClassRegistrationMapper {
+    public static ClassRegistrationResponse toReponse(ClassRegistration reg){
+        return ClassRegistrationResponse.builder()
+                .registrationId(reg.getRegistrationId())
+                .classTitle(reg.getClassEntity().getTitle())
+                .registeredAt(reg.getRegisteredAt())
+                .pending(reg.isPending())
+                .studentEmail(reg.getStudent().getEmail())
+                .build();
+    }
+    
+}
