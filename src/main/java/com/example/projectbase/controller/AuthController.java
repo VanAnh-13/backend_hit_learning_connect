4d55ac6443ccee885dfbbc88aa473ba063a0c73d
Vuong <vanvuong2005827@gmail.com>
2025-07-03 12:22:57 +0700

    fix: workflow of auth

--- src/main/java/com/example/projectbase/controller/AuthController.java
+++ src/main/java/com/example/projectbase/controller/AuthController.java
@@ -5,12 +5,25 @@ import com.example.projectbase.base.VsResponseUtil;
 import com.example.projectbase.constant.UrlConstant;
 import com.example.projectbase.domain.dto.request.auth.LoginRequestDto;
 import com.example.projectbase.domain.dto.request.auth.TokenRefreshRequestDto;
+import com.example.projectbase.domain.dto.request.user.ChangePassFirstTimeRequest;
+import com.example.projectbase.domain.dto.request.user.ChangePassRequest;
+import com.example.projectbase.domain.dto.request.user.GetEmailDto;
+import com.example.projectbase.domain.dto.request.user.VerifyCodeDto;
+import com.example.projectbase.security.CurrentUser;
+import com.example.projectbase.security.UserPrincipal;
 import com.example.projectbase.service.AuthService;
+import com.example.projectbase.service.MailService;
+import com.example.projectbase.service.UserService;
 import com.example.projectbase.validator.annotation.ValidFileImage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -25,6 +38,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class AuthController {
 
   private final AuthService authService;
+
+  private final UserService userService;
+
+  private final MailService mailService;
 
   @Operation(summary = "API Login")
   @PostMapping(UrlConstant.Auth.LOGIN)
@@ -42,4 +59,42 @@ public class AuthController {
   public ResponseEntity<?> refreshToken(@Valid @RequestBody TokenRefreshRequestDto request) {
     return VsResponseUtil.success(authService.refresh(request));
   }
+
+  //change password
+//  @Tag(name = "user-controller")
+  @Operation(summary = "API change password for first time login (only first time)")
+  @PostMapping(UrlConstant.Auth.PASSWORD_CHANGE_FIRST_TIME)
+  public ResponseEntity<?> changePass(@Parameter(name = "principal", hidden = true)
+                                      @CurrentUser UserPrincipal principal,
+                                      @Valid ChangePassFirstTimeRequest changePassFirstTimeRequest) {
+    userService.changePasswordFirstTime(changePassFirstTimeRequest, principal);
+    return VsResponseUtil.success("Successfully changed password");
+  }
+
+//  @Tag(name = "user-controller")
+  @Operation(summary = "API request code to email, get code too much then ban ip for 20 minute")
+  @PostMapping(UrlConstant.Auth.SEND_CODE)
+  public ResponseEntity<?> sendCode(@Parameter(name = "email") @RequestBody GetEmailDto email) throws Exception {
+    return VsResponseUtil.success(mailService.sendMail(email));
+  }
+
+//  @Tag(name = "user-controller")
+  @Operation(summary = "API verify code to change password")
+  @PostMapping(UrlConstant.Auth.VERIFY_CODE)
+  public ResponseEntity<?> verifyCode(@RequestBody VerifyCodeDto verifyCodeDto) throws BadRequestException {
+    return VsResponseUtil.success(mailService.verifyEmail(verifyCodeDto));
+  }
+
+//  @Tag(name = "user-controller")
+  @Operation(summary = "API change password inside account")
+  @PostMapping(UrlConstant.Auth.PASSWORD_CHANGE)
+  public ResponseEntity<?> changePassword(
+          @Parameter(name = "principal", hidden = true)
+          @CurrentUser UserPrincipal principal,
+          @RequestBody @Valid ChangePassRequest changePassRequest
+  ) {
+    return VsResponseUtil.success(userService.changePassword(changePassRequest, principal));
+  }
+
+
 }
