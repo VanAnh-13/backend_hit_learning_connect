bd1edcee5856731bcc57fb11a0cbee3e37e4e14e
Vuong <vanvuong2005827@gmail.com>
2025-07-19 00:18:20 +0700

    fix: getter for ListUserResponseDto

--- src/main/java/com/example/projectbase/domain/dto/response/user/ListUserResponseDto.java
+++ src/main/java/com/example/projectbase/domain/dto/response/user/ListUserResponseDto.java
@@ -1,10 +1,14 @@
 package com.example.projectbase.domain.dto.response.user;
 
-import lombok.Builder;
+import lombok.*;
 
 import java.util.List;
 
 @Builder
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ListUserResponseDto {
     List<UserResponseDto> data;
     Long amountOfAllUsers;
