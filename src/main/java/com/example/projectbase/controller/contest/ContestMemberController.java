a18768fbee48eb22de89873b9603189bae4d6b63
thachpham2103 <phamthach2103@gmail.com>
2025-07-22 14:28:47 +0700

    feat: impl ContestMemberController submit and joinContest

--- src/main/java/com/example/projectbase/controller/contest/ContestMemberController.java
+++ src/main/java/com/example/projectbase/controller/contest/ContestMemberController.java
@@ -2,18 +2,25 @@ package com.example.projectbase.controller.contest;
 
 import com.example.projectbase.base.RestApiV1;
 import com.example.projectbase.constant.ErrorMessage;
+import com.example.projectbase.constant.ResponseMessage;
+import com.example.projectbase.domain.dto.request.contest.ContestSubmissionRequest;
+import com.example.projectbase.domain.dto.response.contest.ContestReponseDto;
 import com.example.projectbase.domain.dto.response.contest.ContestResultResponse;
 import com.example.projectbase.service.ContestService;
 import com.example.projectbase.service.impl.ContestServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RequestMapping("api/contest")
@@ -38,7 +45,82 @@ public class ContestMemberController {
       }
     }
 
-//    @Operation(summary = "Api se")
+    @Operation(summary = "Api search contest")
+    @GetMapping("/search")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> search(@RequestParam String keywork,@RequestParam (defaultValue = "0") int page,
+                                    @RequestParam (defaultValue = "10")int size){
+        try{
+            Page<ContestReponseDto> contestPage= contestService.search(keywork, page, size);
+            return ResponseEntity.ok(contestPage);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessage.Contest.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Api View exam details")
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getContestDetail(@PathVariable Long id){
+        try{
+            ContestReponseDto reponse= contestService.getById(id);
+            return ResponseEntity.ok(reponse);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessage.Contest.CONTEST_DETAIL_NOT_FOUND);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessage.Contest.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Api join contest by contestId")
+    @GetMapping("/join/{contestId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> joinContest(@PathVariable Long contestId){
+        try{
+            contestService.joinContest(contestId);
+            return ResponseEntity.ok(ResponseMessage.Contest.CREATE_SUCCESS);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorMessage.Contest.VALIDATION_FAILED);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessage.Contest.CONTEST_NOT_FOUND);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessage.Contest.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Api start contets")
+    @GetMapping("/start/{contestId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> startContest(@PathVariable Long contestId){
+        try{
+            return ResponseEntity.ok(contestService.startContest(contestId));
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorMessage.Contest.VALIDATION_FAILED);
+
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessage.Contest.CONTEST_NOT_FOUND);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessage.Contest.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Api view submit contest")
+    @PostMapping("/submit")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> submitContest(@ModelAttribute ContestSubmissionRequest request,
+                                           @RequestParam("file")MultipartFile file){
+        try{
+            contestService.submitContest(request,file);
+            return ResponseEntity.ok(ResponseMessage.Contest.SUCCESS);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorMessage.Contest.VALIDATION_FAILED);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessage.Contest.CONTEST_NOT_FOUND);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessage.Contest.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @Operation(summary = "Api result contest by id")
     @GetMapping("{id}/result")
     @PreAuthorize("hasRole('USER')")
