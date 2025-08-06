890806ebe48e5b9fdbe1d8d57d8c5b814916c753
Vuong <vanvuong2005827@gmail.com>
2025-08-06 23:43:27 +0700

    fix: updateContestDto

--- src/main/java/com/example/projectbase/domain/dto/request/contest/ContestUpdateDto.java
+++ src/main/java/com/example/projectbase/domain/dto/request/contest/ContestUpdateDto.java
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 //
-//@Getter
-//@Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -25,7 +25,7 @@ public class ContestUpdateDto {
 //    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime endTime;
 
-    private String fileUrl;
+    private String urlFile;
 
     private double highestScore;
 
@@ -35,67 +35,5 @@ public class ContestUpdateDto {
 
     private String status;
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getFileUrl() {
-        return fileUrl;
-    }
-
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
-    }
-
-    public double getHighestScore() {
-        return highestScore;
-    }
-
-    public void setHighestScore(double highestScore) {
-        this.highestScore = highestScore;
-    }
-
-    public String getResultSummary() {
-        return resultSummary;
-    }
-
-    public void setResultSummary(String resultSummary) {
-        this.resultSummary = resultSummary;
-    }
-
-    public String getRanking() {
-        return ranking;
-    }
-
-    public void setRanking(String ranking) {
-        this.ranking = ranking;
-    }
 }
